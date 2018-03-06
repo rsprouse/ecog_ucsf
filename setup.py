85964a4b7dc5abeca3cb912ab502218f2b5f4dd6
Ronald Sprouse <ronald@berkeley.edu>
2018-03-05 18:47:16 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'ecog_ucsf',
+  packages = ['ecog_ucsf', 'ecog_ucsf.cmu_sphinx'],
+  classifiers = [
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+  ]
+)
